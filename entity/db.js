7543e885121829e959a355fc272304bac2eb3d1e
kaiser24 <damsog38@gmail.com>
2022-04-04 22:55:48 -0500

    Added mongoose and defining the DB

---
+++ entity/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const logger = require('../utils/logger');
+
+mongoose.connect("mongodb://localhost:gnotesdb", ()=>{
+        logger.info( colorText("SERVER DATABASE INFO: Connection has been established succesfully.") );
+    }, (error) => {
+        logger.info( colorText("SERVER DATABASE INFO: Unable to connect to the database: ", error) );
+    }
+)
