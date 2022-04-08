f2797246dfa06f8d8a9432b32492be87f8c7d679
kaiser24 <damsog38@gmail.com>
2022-04-07 22:35:25 -0500

    Small fix. importing colortext util

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
+const colorText = require('../utils/colortext');
 
-mongoose.connect("mongodb://localhost:gnotesdb", ()=>{
+mongoose.connect(`mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_NAME}`, ()=>{
         logger.info( colorText("SERVER DATABASE INFO: Connection has been established succesfully.") );
     }, (error) => {
         logger.info( colorText("SERVER DATABASE INFO: Unable to connect to the database: ", error) );
