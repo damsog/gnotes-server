becd0f94c93b7978cc939638fc2308473c0f91ee
felipe.serna <damsog38@gmail.com>
2022-04-08 11:58:38 -0500

    A small correction to the connection

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 const colorText = require('../utils/colortext');
 
-mongoose.connect(`mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_NAME}`, ()=>{
+mongoose.connect(`mongodb://${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`, ()=>{
         logger.info( colorText("SERVER DATABASE INFO: Connection has been established succesfully.") );
     }, (error) => {
         logger.info( colorText("SERVER DATABASE INFO: Unable to connect to the database: ", error) );
