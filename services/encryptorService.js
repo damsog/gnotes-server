9bec09125db544b870747f0876bf6d644164df13
kaiser24 <damsog38@gmail.com>
2022-04-10 22:45:58 -0500

    Created a new method to encrypt and match passwords

---
+++ services/encryptorService.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs');
+const logger = require('../utils/logger');
+const colorText = require('../utils/colortext');
+const utils = {};
+
+utils.encryptPassword = async (password) => {
+    logger.debug( colorText("Encrypting Password: Password Received. Encrypting") );
+    const salt = await bcrypt.genSalt(10);
+    logger.debug( colorText("Encrypting Password: Salt generated") );
+    const hashedPassword = await bcrypt.hash(password, salt);
+    logger.debug( colorText("Encrypting Password: Password hashed. returning") );
+    return hashedPassword;
+}
+
+utils.matchedPassword = async (password, savedPassword) => {
+    try{
+        logger.debug( colorText("Matching Password: Received. Trying to compare") );
+        const result = await bcrypt.compare(password, savedPassword);
+        logger.debug( colorText("Matching Password: Compared successful. result: "+ result ) );
+        return result;
+    }catch(e) {
+        logger.error( colorText(e) );
+        return 0;
+    }
+}
+
+module.exports = utils;
