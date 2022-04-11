6d1ac3897077aec1f45a4800908f22f2f17f80be
kaiser24 <damsog38@gmail.com>
2022-04-10 22:28:02 -0500

    Added the user routers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const usersController = require('../controllers/userController');
 
 router.get('/create', usersController.createUser );
-
+router.post('/login', usersController.validateUser );
+router.get('/getAllUsers', usersController.getAllUsers );
+router.get('/getUserById', usersController.getUserById );
+router.get('/getUserByUsername', usersController.getUserByUsername );
+router.get('/getUserByEmail', usersController.getUserByEmail );
+router.put('/updateUser', usersController.updateUser );
+router.delete('/deleteUser', usersController.deleteUser );
 module.exports = router;
