e9202e63dcd6bcbc0952daa84c19a492717cea6a
kaiser24 <damsog38@gmail.com>
2022-04-12 21:46:16 -0500

    Added authentication service middleware to intercept requests and check the jwt

---
+++ services/authenticationService.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const logger = require('../utils/logger');
+const colorText = require('../utils/colortext');
+
+
+const verifyToken = (req, res, next) => {
+    const tokenBearer = req.body.token || req.query.token || req.headers["x-access-token"] || req.headers["authorization"];
+    logger.debug( colorText(`AUTH MIDDLEWARE: token received: ${tokenBearer}`) );
+
+    if(!tokenBearer) {
+        logger.debug( colorText(`AUTH MIDDLEWARE: no token found`) );
+        return res.status(403).send("A token is required for authentication");
+    }
+    try {
+        const token = tokenBearer.split("Bearer ")[1];
+        logger.debug( colorText(`AUTH MIDDLEWARE: Verifying token: ${token}`) );
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        
+        logger.debug( colorText(`AUTH MIDDLEWARE: token validated`) );
+        req.user = decoded;
+    } catch (e) {
+
+        logger.debug( colorText(`AUTH MIDDLEWARE: Invalid token`) );
+        return res.status(401).send("Invalid Token");
+    }
+    return next();
+}
+
+module.exports = verifyToken;
