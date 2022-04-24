d58084168a6dfb6173091f85b0e5d12fe3d708e7
kaiser24 <damsog38@gmail.com>
2022-04-24 18:12:34 -0500

    Defined swagger ui for access endpoints

--- routes/accessRoutes.js
+++ routes/accessRoutes.js
@@ -2,7 +2,73 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/userController');
 
+
+/**
+ * @swagger
+ * /api/access/create:
+ *  post:
+ *      summary: Create a new user
+ *      tags: [Access]
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/user'
+ *      responses:
+ *          200:
+ *              description: list of all users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/user'
+ *                                
+ */
 router.post('/create', usersController.createUser );
+
+/**
+ * @swagger
+ * /api/access/login:
+ *  post:
+ *      summary: Login
+ *      tags: [Access]
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/user_access'
+ *      responses:
+ *          200:
+ *              description: Access response with token
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/user'
+ *                                
+ */
 router.post('/login', usersController.validateUser );
+
+ /**
+ * @swagger
+ * components:
+ *  schemas:
+ *      user_access:
+ *          type: object
+ *          required:
+ *              - username or email
+ *              - password
+ *          properties:
+ *              username:
+ *                  type: string
+ *                  description: Username set by user
+ *              password:
+ *                  type: string
+ *                  description: key to access
+*/
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,69 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/userController');
 
+/**
+ * @swagger
+ * /api/users/getAllUsers:
+ *  get:
+ *      summary: Return all users
+ *      security:
+ *          - bearerAuth: []
+ *      tags: [Users]
+ *      responses:
+ *          200:
+ *              description: list of all users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/user'
+ *                                
+ */
 router.get('/getAllUsers', usersController.getAllUsers );
 router.get('/getUserById', usersController.getUserById );
 router.get('/getUserByUsername', usersController.getUserByUsername );
 router.get('/getUserByEmail', usersController.getUserByEmail );
 router.put('/updateUser', usersController.updateUser );
 router.delete('/deleteUser', usersController.deleteUser );
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      user:
+ *          type: object
+ *          required:
+ *              - username
+ *              - password
+ *          properties:
+ *              id:
+ *                  type: int
+ *                  description: The auto-generated id of the user
+ *              email:
+ *                  type: string
+ *                  description: Email set by user
+ *              username:
+ *                  type: string
+ *                  description: Username set by user
+ *              password:
+ *                  type: string
+ *                  description: key to access
+ *              first_name:
+ *                  type: string
+ *                  description: first name of the user
+ *              last_name:
+ *                  type: string
+ *                  description: last name of the user
+ *              time_register:
+ *                  type: string
+ *                  description: time
+ *              createdAt:
+ *                  type: string
+ *                  description: time
+ *              updatedAt:
+ *                  type: string
+ *                  description: time
+*/
+
 module.exports = router;
