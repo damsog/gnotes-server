a475a49c52d39e7179de5dfbfa7575f463f41656
kaiser24 <damsog38@gmail.com>
2022-05-21 15:59:29 -0500

    Created userToCreate Schema for swagger

--- routes/accessRoutes.js
+++ routes/accessRoutes.js
@@ -14,7 +14,7 @@ const usersController = require('../controllers/userController');
  *          content: 
  *              application/json:
  *                  schema:
- *                      $ref: '#/components/schemas/user'
+ *                      $ref: '#/components/schemas/userToCreate'
  *      responses:
  *          200:
  *              description: list of all users
@@ -71,4 +71,31 @@ router.post('/login', usersController.validateUser );
  *                  description: key to access
 */
 
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      userToCreate:
+ *          type: object
+ *          required:
+ *              - username
+ *              - password
+ *          properties:
+ *              email:
+ *                  type: string
+ *                  description: Email set by user
+ *              username:
+ *                  type: string
+ *                  description: Username set by user
+ *              password:
+ *                  type: string
+ *                  description: key to access
+ *              first_name:
+ *                  type: string
+ *                  description: first name of the user
+ *              last_name:
+ *                  type: string
+ *                  description: last name of the user
+*/
 module.exports = router;
