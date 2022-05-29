2399e32f801a5a0d509c49b9715ff6d1ee2af004
kaiser24 <damsog38@gmail.com>
2022-05-29 15:47:29 -0500

    Working on parsing filters

--- services/objectService.js
+++ services/objectService.js
@@ -13,6 +13,37 @@ resultStructure = (operation) => {
     }
 }
 
+parseFilters = (string_filters) => {
+    let filters_json = {};
+    let others_list = [];
+
+    const filters_array = string_filters.split("|");
+    
+    for (const entry of filters_array) {
+        let entry_array = entry.split(":");
+
+        // TODO: Check if Keyval is valid. Checks here if DB injection
+ 
+        // Check if more than 2 values
+        if (entry_array.length > 2) throw new Error("Filter not understood.");
+        
+        // Check if less than 2 values
+        if (entry_array.length < 2) others_list.push(entry_array[0]);
+
+        // If has exactly 2 elements
+
+        // If the second element is an array
+        if ( (entry_array[1].substring(0,1) === "[") && (entry_array[1].substring(entry_array[1].length-1,entry_array[1].length) ) ){
+            data = entry_array[1].substring(1,entry_array[0]-1).split(",");
+        }else{
+            data = entry_array[1];
+        }
+
+        filters_json[entry_array[0]] = data;
+        
+    }
+}
+
 exports.createObject = async (options) => {
     const operation = "Create Object";
     logger.debug( colorText(`${operation} with options ${JSON.stringify(options)}`) );
