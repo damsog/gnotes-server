37183e9a79df11d97d5974375aa9a16419aba736
felipe.serna <damsog38@gmail.com>
2022-06-01 10:23:19 -0500

    Separated options parser as a util function and made it more general

--- services/objectService.js
+++ services/objectService.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel');
 const logger = require('../utils/logger');
 const colorText = require('../utils/colortext');
 const { ObjectId } = require('mongoose').Types;
+const optionsParser = require('../utils/optionsParser');
 
 resultStructure = (operation) => { 
     return result = {
@@ -11,40 +12,6 @@ resultStructure = (operation) => {
         message: "",
         data: ""
     }
-}
-
-parseFilters = (string_filters) => {
-    let filters_json = {};
-    let others_list = [];
-
-    const filters_array = string_filters.split("|");
-    
-    for (const entry of filters_array) {
-        let entry_array = entry.split(":");
-
-        // TODO: Check if Keyval is valid. Checks here if DB injection
- 
-        // Check if more than 2 values
-        if (entry_array.length > 2) throw new Error("Filter not understood.");
-        
-        // Check if less than 2 values
-        if (entry_array.length < 2){ others_list.push(entry_array[0]); continue };
-
-        // If has exactly 2 elements
-        // If the second element is an array
-        if ( (entry_array[1].substring(0,1) === "[") && (entry_array[1].substring(entry_array[1].length-1,entry_array[1].length) === "]" ) ){
-            data = entry_array[1].substring(1,entry_array[1].length-1).split(",");
-        }else{
-            data = entry_array[1];
-        }
-
-        filters_json[entry_array[0]] = data;
-        
-    }
-
-    filters_json["others"] = others_list;
-
-    return filters_json;
 }
 
 exports.createObject = async (options) => {
@@ -70,7 +37,7 @@ exports.createObject = async (options) => {
 
         // TODO: Other Validations
         // Parsing the filters
-        options.filters = parseFilters(options.filters);
+        options.filters = optionsParser(options.filters);
 
         // Create a new object
         logger.debug( colorText("Creating new object") );
---
+++ utils/optionsParser.js
@@ -0,0 +1,38 @@
+const  optionsParser = (string_filters, allow_lists = true) => {
+    let filters_json = {};
+    let others_list = [];
+
+    const filters_array = string_filters.split("|");
+    
+    for (const entry of filters_array) {
+        let entry_array = entry.split(":");
+
+        // TODO: Check if Keyval is valid. Checks here if DB injection
+ 
+        // Check if more than 2 values
+        if (entry_array.length > 2) throw new Error("Filter not understood.");
+        
+        // Check if less than 2 values
+        if (entry_array.length < 2){ others_list.push(entry_array[0]); continue };
+
+        // If has exactly 2 elements
+        // If the second element is an array
+        if ( ( (entry_array[1].substring(0,1) === "[") && 
+               (entry_array[1].substring(entry_array[1].length-1,entry_array[1].length) === "]" ) ) &&
+               allow_lists ){
+
+            data = entry_array[1].substring(1,entry_array[1].length-1).split(",");
+        }else{
+            data = entry_array[1];
+        }
+
+        filters_json[entry_array[0]] = data;
+        
+    }
+
+    filters_json["others"] = others_list;
+
+    return filters_json;
+}
+
+module.exports = optionsParser;
